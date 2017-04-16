4486c9ccfceefb573e49e5e6209345973df3de83
dafr9205 <dafr9205@colorado.edu>
2017-04-15 22:17:53 -0600

    created decimalToDegree func. to prepare for interactivity merge

--- piecharts/sketch.js
+++ piecharts/sketch.js
@@ -1,6 +1,15 @@
-var data = [48.96,47.268,27.18,156.78,11.3,68.512];
-var data1 = [85.212,54.144,35.1,23.364,31.716,130.464];
-var data2 = [94.5,50.796,46.512,12.168,29.844,126.18];
+
+var fileName = "overviewData.csv";
+var csvdata;
+
+
+// var data = [48.96,47.268,27.18,156.78,11.3,68.512];
+// var data1 = [85.212,54.144,35.1,23.364,31.716,130.464];
+// var data2 = [94.5,50.796,46.512,12.168,29.844,126.18];
+
+var data = [];
+var data1 = [];
+var data2 = [];
 
 var piedata = [], piecolor = ['rgb(82,70,86)','rgb(207,71,71)', 'rgb(234,122,88)','rgb(228,220,203)','rgb(77,244,255)','rgb(25,255,205)'];
 var piedata1 =[],  piecolor1 = ['rgb(82,70,86)','rgb(207,71,71)', 'rgb(234,122,88)','rgb(228,220,203)','rgb(77,244,255)','rgb(25,255,205)'];
@@ -10,9 +19,28 @@ var mouseAngle = 0, pieDelta = 0, hover = 0;
 var mouseAngle1 = 0;
 var mouseAngle2 = 0;
 
-var fileName = "overviewData.csv"
 
-function setup() {
+var dataA = [.1360,.1313,.0755,.4355,.0314,.1903];
+var dataB = [.2367,.1504,.0975,.0649,.0881,.3624];
+var dataC = [.2625,.1411,.1292,.0338,.0829,.3505];
+
+
+function decimalToDegrees(dataVals){
+  var dataAngles = [];
+  for(i=0; i<dataVals.length; i++){
+    append(dataAngles, dataVals[i]*360);
+  }
+  return dataAngles;
+}
+
+
+
+function setup()
+{
+  data = decimalToDegrees(dataA);
+  data1 = decimalToDegrees(dataB);
+  data2 = decimalToDegrees(dataC);
+
   createCanvas(1000,600);
   background(100);
   total = data.reduce(function(a,b){ return a+b; }, 0);
@@ -20,13 +48,16 @@ function setup() {
   total2 = data2.reduce(function(a,b){ return a+b; }, 0);
 
 
+
+  console.log(data);
+
   for(var i=0,count=0;i<data.length;i++) {
     piedata.push([Math.PI * 2 * count / total, Math.PI * 2 * (count + data[i]) / total]);
     count += data[i];
   }
 
   for(var i=0,count=0;i<data1.length;i++) {
-    piedata1.push([Math.PI * 2 * count / total1, Math.PI * 2 * (count + data1[i]) / total1]); 
+    piedata1.push([Math.PI * 2 * count / total1, Math.PI * 2 * (count + data1[i]) / total1]);
     count += data1[i];
   }
 
@@ -36,6 +67,8 @@ function setup() {
   }
 
 }
+
+
 
 function draw() {
 background(100);
