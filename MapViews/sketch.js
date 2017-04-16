7446aa527c46d507e79b3d047c2b1535d88b2c2b
Lila Finch <lila@rl1-142-71-dhcp.int.colorado.edu>
2017-04-16 16:36:33 -0600

    maps with dict issues

--- MapViews/sketch.js
+++ MapViews/sketch.js
@@ -85,15 +85,35 @@ var boxi = [false,false,false,false,false];
 var sliderX;
 var sliderYs;
 var originalSliderYs;
-var sliderWidth = 30;
+var sliderWidth = 15;
 var sliderHeight = 10;
 var isOverSlider = false;
 var onSlideri;
 var overView = true;
 var mapView = false;
+var box4Counter;
 
 // variables for pie charts
-var pieRowIndicies = [];
+var pieColors = ['rgb(82,70,86)','rgb(207,71,71)',
+                 'rgb(234,122,88)','rgb(228,220,203)',
+                 'rgb(77,244,255)','rgb(25,255,205)'];
+var mouseAngle = 0, pieDelta = 0, hover = 0;
+var pieCenterX;
+var verticalDetailSpace = 100;
+var pieDiameter = 130;
+var spaceBetweenPieCenters;
+var pieCenterYs = [];
+var industry = [0.1,0.2,0.3,0.2,0.1,0.1];
+var productGroup = [0.1,0.2,0.3,0.2,0.1,0.1];
+var buildingType = [0.1,0.2,0.3,0.2,0.1,0.1];
+var industryLabels = ['cat1','cat2','cat3','cat4','cat5','cat6'];
+var productGroupLabels = ['cat1','cat2','cat3','cat4','cat5','cat6'];
+var buildingTypeLabels = ['cat1','cat2','cat3','cat4','cat5','cat6'];
+var pieSectionLabels = [industryLabels,productGroupLabels,buildingTypeLabels];
+var pieDecimalData = [industry,productGroup,buildingType];
+var pieLabels = ['Industry','Product Group','Building Type'];
+var pieCounter;
+var pieDictBuildType = {};
 
 function preload() {
   denver = loadTable(denverFile, "csv", "header");
@@ -132,7 +152,7 @@ function setup() {
   mapCenterY = verticalSpace+mapSize/2;
   mapTextX = overviewSpace+horizontalSpace+mapSize;
   mapTextY = verticalSpace-verticalSpace/6;
-  sliderX = overviewSpace+horizontalSpace+mapSize+(width-overviewSpace-detailSpace-horizontalSpace-mapSize)/3;
+  sliderX = overviewSpace+horizontalSpace+mapSize+(width-overviewSpace-detailSpace-horizontalSpace-mapSize)/4.7;
   sliderYs = [verticalSpace+mapSize/6, verticalSpace+mapSize-mapSize/6];
   originalSliderYs = [verticalSpace+mapSize/6, verticalSpace+mapSize-mapSize/6];
   sliderLineYs = [verticalSpace+mapSize/6, verticalSpace+mapSize-mapSize/6];
@@ -150,6 +170,13 @@ function setup() {
     originalEdgeLat[i] = invMercY(-mapSize/2.5,zoomNames[i],centerY);
     originalEdgeLon[i] = invMercX(-mapSize/2.5,zoomNames[i],centerX);
   }
+
+  // pie chart locations
+  spaceBetweenPieCenters = (height-verticalDetailSpace)/3;
+  pieCenterYs = [verticalDetailSpace+spaceBetweenPieCenters/3,
+                 verticalDetailSpace+spaceBetweenPieCenters+spaceBetweenPieCenters/3,
+                 verticalDetailSpace+spaceBetweenPieCenters*2+spaceBetweenPieCenters/3];
+  pieCenterX = (width-detailSpace)+detailSpace/2+(detailSpace-pieDiameter)/4;
 
 }
 
@@ -269,7 +296,6 @@ function draw() {
   }
   fill(200);
   noStroke();
-  textAlign(LEFT,CENTER);
   textSize(12);
   textStyle(NORMAL);
   if(checkBox[0]){
@@ -280,17 +306,18 @@ function draw() {
     var showTextMin = minCost;
     var showTextMax = maxCost;
   }
-  text(showTextMax, sliderX+20, sliderYs[0]+5);
-  text(showTextMin, sliderX+20, sliderYs[1]+5);
+  textAlign(LEFT,CENTER);
+  text("$"+nfc(showTextMax), sliderX+sliderWidth/2+4, sliderYs[0]+sliderHeight/2);
+  text("$"+nfc(showTextMin), sliderX+sliderWidth/2+4, sliderYs[1]+sliderHeight/2);
        
   // boxes for filters on map
   var boxSize = 20;
   var boxesX = overviewSpace+horizontalSpace*5/6-boxSize;
   var boxesYs = [verticalSpace,
-                 (height-((height-verticalSpace-mapSize)+mapImageSize+verticalSpace))/5+verticalSpace,
+                 (height-((height-verticalSpace-mapSize)+mapImageSize+verticalSpace))/5+verticalSpace-10,
                  (height-((height-verticalSpace-mapSize)+mapImageSize+verticalSpace))*2/5+verticalSpace,
-                 (height-((height-verticalSpace-mapSize)+mapImageSize+verticalSpace))*3/5+verticalSpace,
-                 (height-((height-verticalSpace-mapSize)+mapImageSize+verticalSpace))*4/5+verticalSpace,];
+                 (height-((height-verticalSpace-mapSize)+mapImageSize+verticalSpace))*3/5+verticalSpace-10,
+                 (height-((height-verticalSpace-mapSize)+mapImageSize+verticalSpace))*4/5+verticalSpace];
   var checkBoxLabels = ['PROFIT','COST','On Network','Open Opportunity','Details of Selection'];
   for(i=0; i<boxesYs.length; i++){
     stroke(200);
@@ -305,6 +332,9 @@ function draw() {
         }
         else if(!checkBox[i] && (i>1)){
           checkBox[i] = true;
+          if(checkBox[4]){
+            box4Counter = counter;
+          }
         }
         else if(!checkBox[i] && (i<=1)){
           if(i===0){
@@ -327,13 +357,24 @@ function draw() {
       else if(checkBox[1] && i===1){
         fill(255,0,255,100);
       }
+      else if(checkBox[4] && i===4){
+        fill(50);
+      }
       else {
         noFill();
       }
     }
 
-    rect(boxesX,boxesYs[i],boxSize,boxSize);
-    if(checkBox[i] && i>1){
+    if(i<2){
+      triangle(boxesX,boxesYs[i],boxesX,boxesYs[i]+boxSize,boxesX+boxSize,boxesYs[i]+boxSize/2);
+    }
+    else if(i>=2 && i<4){
+      rect(boxesX,boxesYs[i],boxSize,boxSize);
+    }
+    else if(i===4){
+      ellipse(boxesX+boxSize/2,boxesYs[i]+boxSize/2,boxSize,boxSize);
+    }
+    if(checkBox[i] && i>1 && i<4){
       if(i===2){
         stroke(255,255,0);
       }
@@ -349,7 +390,12 @@ function draw() {
     textSize(16);
     textAlign(RIGHT,CENTER);
     text(checkBoxLabels[i], boxesX-15, boxesYs[i]+boxSize/2);
+    text("or", boxesX-15, boxesYs[0]+(boxesYs[1]-boxesYs[0])*5/7);
   }
+  stroke(200);
+  strokeWeight(1);
+  line(smallMapLeftX,boxesYs[1]+boxSize+20,smallMapLeftX+mapImageSize,boxesYs[1]+boxSize+20);
+  line(smallMapLeftX,boxesYs[3]+boxSize+20,smallMapLeftX+mapImageSize,boxesYs[3]+boxSize+20);
   
   //use web mercator equations to plot points on map
   isOverMap = mouseOverMap();
@@ -362,9 +408,64 @@ function draw() {
   text(markName[market],overviewSpace+horizontalSpace+mapSize,verticalSpace/2);
 
   if(checkBox[4]){
+    
     textAlign(CENTER,CENTER);
-    textSize(20);
-    text('Pie Charts!',width-detailSpace/2,100);
+    textSize(30);
+    fill(200);
+    text('TOP 5',width-detailSpace/2,verticalDetailSpace/3);
+    
+    if(box4Counter === counter){
+      var pieDataOut = getPieData(pieDictBuildType);
+      pieSectionLabels[2] = pieDataOut[0];
+      pieDecimalData[2] = pieDataOut[1];
+      console.log(pieDictBuildType);
+    }
+    
+    var data;
+    var total;
+    var piedata = [];
+    for(m=0; m<pieCenterYs.length; m++){
+      data = decimalToDegrees(pieDecimalData[m]);
+      total = data.reduce(function(a,b){ return a+b; }, 0);
+      for(var j=0,count=0;j<data.length;j++) {
+        piedata.push([Math.PI * 2 * count / total, Math.PI * 2 * (count + data[j]) / total]);
+        count += data[j];
+      }
+      // draw pie charts
+      for(var n=0,dx=0,dy=0;n<piedata.length;n++,dx=0,dy=0) {
+        if(mouseX > (pieCenterX-pieDiameter/2) && mouseX < (pieCenterX+pieDiameter/2) && 
+           mouseY > (pieCenterYs[m]-pieDiameter/2) && mouseY < (pieCenterYs[m]+pieDiameter/2)) {
+          pieCounter = m;
+        }
+        if(mouseAngle >= piedata[n][0] && mouseAngle < piedata[n][1] && pieCounter===m && mouseX > (pieCenterX-pieDiameter/2) && mouseX < (pieCenterX+pieDiameter/2) && 
+           mouseY > (pieCenterYs[m]-pieDiameter/2) && mouseY < (pieCenterYs[m]+pieDiameter/2)) {
+          dx = Math.cos((piedata[n][0] + piedata[n][1])/2) * 10;
+          dy = Math.sin((piedata[n][0] + piedata[n][1])/2) * 10;
+          noStroke();
+          textSize(15);
+          textAlign(CENTER,TOP);
+          fill(200);
+          text((pieSectionLabels[m][n%6]+", "+pieDecimalData[m][n%6]*100+"%"),pieCenterX,pieCenterYs[m]+pieDiameter/2+15);
+          stroke(200);
+          strokeWeight(2);
+        }
+        else {
+          noStroke();
+        }
+        fill(pieColors[n%6]);
+        arc(pieCenterX + dx, pieCenterYs[m] + dy, pieDiameter, pieDiameter, piedata[n][0], piedata[n][1], PIE);
+      }
+      // draw titles
+      push()
+      translate((width-detailSpace)+(detailSpace-pieDiameter)/3,pieCenterYs[m]);
+      textSize(20);
+      textAlign(CENTER,CENTER);
+      noStroke();
+      fill(200);
+      rotate(-PI/2);
+      text(pieLabels[m],0,0)
+      pop()
+    }
   }
 
   counter += 1;
@@ -448,7 +549,15 @@ function mapGraphs(mapCenterX,mapCenterY,clon,clat,zoom){
             val = int(map(cost,0,originalMaxCost,0,255));
             fill(255,val,255,150);
             ellipse(x, y, radius);
-            // append(pieRowIndicies,i);
+            var btype = row.get('BuildingType')
+
+                    if (btype in pieDictBuildType)
+                    {
+                        pieDictBuildType[btype] += 1;
+                    }
+
+                    else {pieDictBuildType[btype] = 1}
+            
           }
         }
     }
@@ -569,8 +678,8 @@ function mousePressed() {
 
 function mouseDragged() {
   if(locked){
-    var amountX = map(xOffset-mouseX,-100,100,-0.1,0.1)
-    var amountY = map(yOffset-mouseY,100,-100,-0.1,0.1)
+    var amountX = map(xOffset-mouseX,-100,100,-0.1,0.1);
+    var amountY = map(yOffset-mouseY,100,-100,-0.1,0.1);
     
     // check if new lat and long edges are in range
     var centerX = mercX(clon+amountX, zoom);
@@ -652,7 +761,6 @@ function mouseDragged() {
       }
     }
   }
-  
 }
 
 function mouseReleased() {
@@ -689,4 +797,77 @@ function mouseOverASlider() {
   else {
     return false;
   }
+}
+
+function decimalToDegrees(dataVals){
+  var dataAngles = [];
+  for(i=0; i<dataVals.length; i++){
+    append(dataAngles, dataVals[i]*360);
+  }
+  return dataAngles;
+}
+
+function mouseMoved() {
+  mouseAngle = Math.PI / 2 - Math.atan((pieCenterX - mouseX) / (pieCenterYs[pieCounter] - mouseY));
+  if(mouseY < pieCenterYs[pieCounter]) mouseAngle = mouseAngle + Math.PI;
+}
+
+function getPieData(dict)
+{
+    //takes dictionary and gives you percents with labels that share index
+        /*
+          var pieDictIndustry = {}
+          var pieDictProdGroup = {}
+          var pieDictBuildType = {}
+        */
+
+    var toSort = [];
+    var names = [];
+    var values = [];
+
+    for (var keys in dict)
+    {
+        toSort.push([keys, dict[keys]]);
+    }
+
+
+    var sorted = toSort.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    var sum = 0;
+
+    //find sum for percentages
+
+    for (var i = 0; i < sorted.length; i++)
+    {
+        sum += sorted[i][1];
+    }
+
+    //get top five, divide for percent
+    var othersSum = 0;
+
+    //fill in output list
+    for (i = 0; i < sorted.length; i++)
+    {
+        if (i < 5)
+        {
+            names.push(sorted[i][0]);
+            values.push((sorted[i][1] / sum))
+        }
+
+        else if (i = 5)
+        {
+            names.push("Other")
+            othersSum += sorted[i][1]
+        }
+
+        else
+        {
+            othersSum += sorted[i][1]
+        }
+
+    }
+    values[5] =(othersSum / sum) ;
+    return [names, values]
 }
